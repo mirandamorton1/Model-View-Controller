94d520eeb314a96afc9eb7d1d5f47a02500073be
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-08-03 16:45:30 -0500

    auth added

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,10 @@
 const withAuth = (req, res, next) => {
-    //conditional that check sif the user is logged in,
-    // if not-send to the login page
-    // else - next();
-}
-
-// export withAuth
+    
+    if(!req.session.logged_in) {
+      res.redirect('/login')
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
