f6db734b57c0bda52d0ad0211902e2d53bb81a74
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-08-03 16:41:20 -0500

    connnection added

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,25 @@
 //require sequelize
+const Sequelize = require('sequelize');
 
 //dotenv and config
+require('dotenv').config();
 
-//creat connection to the database
+//creat connection to the database - mini project
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
